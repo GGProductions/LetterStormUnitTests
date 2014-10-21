356f37e4fd7cf32b2f596e685d0ce987ebb38bfc
davelustig <davelustig@users.noreply.github.com>
2014-10-21 16:14:06 -0400

    Update text case for CreateSampleLesson

--- Assetts/Classes/GGProduction/LetterStorm/Data/Collections/LessonBookTests.cs
+++ Assetts/Classes/GGProduction/LetterStorm/Data/Collections/LessonBookTests.cs
@@ -12,18 +12,19 @@ namespace Test.GGProductions.LetterStorm.Data.Collections
         {
             // Create a lesson book with a sample lesson
             LessonBook lessonBook = new LessonBook();
-            lessonBook.CreateSampleLesson();
+            lessonBook.CreateSampleLessons();
 
             // Verify a lesson was created and that it has the correct name
-            Assert.AreEqual(1, lessonBook.Lessons.Count);
-            Assert.AreEqual("Sample Lesson", lessonBook.Lessons[0].Name);
+            Assert.AreEqual(5, lessonBook.Lessons.Count);
+            Assert.AreEqual("K5 Sample", lessonBook.Lessons[0].Name);
 
             // Verify the lesson's words were created
-            Assert.AreEqual(4, lessonBook.Lessons[0].Words.Count);
+            Assert.AreEqual(5, lessonBook.Lessons[0].Words.Count);
             Assert.AreEqual("cat", lessonBook.Lessons[0].Words[0].Text);
             Assert.AreEqual("dog", lessonBook.Lessons[0].Words[1].Text);
-            Assert.AreEqual("fish", lessonBook.Lessons[0].Words[2].Text);
-            Assert.AreEqual("horse", lessonBook.Lessons[0].Words[3].Text);
+            Assert.AreEqual("bug", lessonBook.Lessons[0].Words[2].Text);
+            Assert.AreEqual("fish", lessonBook.Lessons[0].Words[3].Text);
+            Assert.AreEqual("horse", lessonBook.Lessons[0].Words[4].Text);
         }
     }
 }
